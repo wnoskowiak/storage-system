319a7a70f8be71f0bf8f2f9d826b0d6764cc98dc
Wojciech Noskowiak <wojtek.noskowiak@gmail.com>
2023-11-02 17:44:51 -0100

    first commit

---
+++ cp2023/solution/DeviceImpl.java
@@ -0,0 +1,215 @@
+package cp2023.solution;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.Semaphore;
+import cp2023.base.ComponentId;
+import cp2023.base.DeviceId;
+
+public class DeviceImpl {
+
+    private final int numberOfSlots;
+    public final DeviceId id;
+    private final StorageSystemImpl system;
+    private final Set<ComponentId> memory;
+    private final Set<ComponentId> reservations = new HashSet<ComponentId>();
+    private final TransferManager outgoing = new TransferManager();
+    private final Semaphore memoryMutex = new Semaphore(1, true);
+    private final Semaphore outgoingMutex = new Semaphore(1, true);
+    private final Semaphore reservationsMutex = new Semaphore(1, true);
+    public final ArriveOrReserve arriveOrReserve = new ArriveOrReserve();
+    public final AddOrRemove addOrRemove = new AddOrRemove();
+
+    public DeviceImpl(DeviceId id, int numberOfSlots, Set<ComponentId> initialComponentIds, StorageSystemImpl system) {
+
+        this.numberOfSlots = numberOfSlots;
+        this.memory = new HashSet<ComponentId>(initialComponentIds);
+        this.system = system;
+        this.id = id;
+
+    }
+
+    public boolean doIHave(ComponentId elem) throws InterruptedException {
+
+        memoryMutex.acquire();
+
+        boolean result = memory.contains(elem);
+
+        memoryMutex.release();
+        return result;
+    }
+
+    public boolean isBeingTransferred(ComponentId elem) throws InterruptedException {
+
+        outgoingMutex.acquire();
+        reservationsMutex.acquire();
+
+        boolean result = outgoing.amIHere(elem) || reservations.contains(elem);
+
+        reservationsMutex.release();
+        outgoingMutex.release();
+        return result;
+
+    }
+
+    public Set<DeviceId> getDestinations() throws InterruptedException {
+
+        outgoingMutex.acquire();
+
+        Set<DeviceId> result = outgoing.getDeviceIds();
+
+        outgoingMutex.release();
+        return result;
+    }
+
+    public class AddOrRemove {
+
+        public synchronized void remove(ComponentId elem, boolean deletion) throws InterruptedException {
+            memoryMutex.acquire();
+            outgoingMutex.acquire();
+
+            if (deletion) {
+                system.acquireMutex();
+            }
+
+            memory.remove(elem);
+            outgoing.removeTransfer(elem);
+
+            if (deletion) {
+                system.removeComponents(elem);
+            }
+
+            memoryMutex.release();
+            outgoingMutex.release();
+            if (deletion) {
+                system.releaseMutex();
+            }
+
+            notify();
+        }
+
+        public synchronized void add(ComponentId elem, boolean deletion) throws InterruptedException {
+            memoryMutex.acquire();
+            reservationsMutex.acquire();
+
+            if (memory.size() == numberOfSlots) {
+                memoryMutex.release();
+                reservationsMutex.release();
+                wait();
+                memoryMutex.acquire();
+                reservationsMutex.acquire();
+            }
+
+            memory.add(elem);
+            reservations.remove(elem);
+
+            memoryMutex.release();
+            reservationsMutex.release();
+        }
+
+    }
+
+    public class ArriveOrReserve {
+
+        public synchronized void release(ComponentId elem, DeviceId dev, boolean addition) throws InterruptedException {
+
+            outgoingMutex.acquire();
+
+            outgoing.addTransfer(elem, dev);
+
+            outgoingMutex.release();
+
+            System.out.println("===== " + Thread.currentThread().getId() + " has sent a notify"
+                    + " ===");
+
+            notify();
+
+        }
+
+        public synchronized void reserve(ComponentId elem, boolean addition) throws InterruptedException {
+
+            reservationsMutex.acquire();
+            outgoingMutex.acquire();
+            memoryMutex.acquire();
+
+            if (numberOfSlots - memory.size() + outgoing.size() - reservations.size() <= 0) {
+
+                reservationsMutex.release();
+                outgoingMutex.release();
+                memoryMutex.release();
+
+                boolean isCycle = false;
+
+                Set<DeviceId> visited = new HashSet<DeviceId>();
+                Set<DeviceId> next = new HashSet<DeviceId>();
+                Set<DeviceImpl> temp = new HashSet<DeviceImpl>();
+
+                next.addAll(getDestinations());
+
+                System.out.println(next);
+
+                while (true) {
+
+                    temp = system.getDevices(next);
+
+                    next.clear();
+
+                    for (DeviceImpl el : temp) {
+                        if (el != null) {
+                            next.addAll(el.getDestinations());
+                        }
+                    }
+
+                    next.removeAll(visited);
+
+                    System.out.println(next);
+
+                    if (next.isEmpty()) {
+                        break;
+                    }
+
+                    if (next.contains(id)) {
+                        isCycle = true;
+                        break;
+                    }
+
+                    visited.addAll(next);
+
+                }
+
+                if (!isCycle) {
+                    System.out.println("===== " + Thread.currentThread().getId() + " is waiting"
+                            + " ===");
+                    // System.out.println(this.id.toString());
+                    wait();
+                    System.out.println("===== " + Thread.currentThread().getId() + " is going"
+                            + " ===");
+                } else {
+                    System.out.println("===== " + Thread.currentThread().getId() + " no i chuj no i cześć"
+                            + " ===");
+                }
+
+                reservationsMutex.acquire();
+                outgoingMutex.acquire();
+                memoryMutex.acquire();
+
+            }
+
+            if (addition) {
+                system.acquireMutex();
+                system.addComponent(elem);
+            }
+
+            reservations.add(elem);
+
+            if (addition) {
+                system.releaseMutex();
+            }
+
+            reservationsMutex.release();
+            outgoingMutex.release();
+            memoryMutex.release();
+
+        }
+    }
+}
---
+++ cp2023/solution/StorageSystemImpl.java
@@ -0,0 +1,244 @@
+package cp2023.solution;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.Semaphore;
+
+import cp2023.base.ComponentId;
+import cp2023.base.ComponentTransfer;
+import cp2023.base.DeviceId;
+import cp2023.base.StorageSystem;
+import cp2023.exceptions.ComponentAlreadyExists;
+import cp2023.exceptions.ComponentDoesNotExist;
+import cp2023.exceptions.ComponentDoesNotNeedTransfer;
+import cp2023.exceptions.ComponentIsBeingOperatedOn;
+import cp2023.exceptions.DeviceDoesNotExist;
+import cp2023.exceptions.IllegalTransferType;
+import cp2023.exceptions.TransferException;
+
+enum arriveOrReserve {
+    RESERVE,
+    RELEASE
+}
+
+enum addOrRemove {
+    ADD,
+    REMOVE
+}
+
+enum operation {
+    INSERT,
+    DELETE,
+    TRANSFER
+}
+
+public class StorageSystemImpl implements StorageSystem {
+
+    private final Map<DeviceId, DeviceImpl> devices;
+    private final Set<ComponentId> components;
+    private final Semaphore componentMutex = new Semaphore(1, true);
+    private final Set<ComponentId> underTransfer = new HashSet<ComponentId>();
+    private final Semaphore underTransferMutex = new Semaphore(1, true);
+
+    public StorageSystemImpl(Map<DeviceId, Integer> deviceTotalSlots,
+            Map<ComponentId, DeviceId> componentPlacement) {
+
+        this.devices = new HashMap<DeviceId, DeviceImpl>();
+        this.components = new HashSet<ComponentId>();
+
+        Map<DeviceId, Set<ComponentId>> temp = new HashMap<DeviceId, Set<ComponentId>>();
+
+        for (Map.Entry<ComponentId, DeviceId> entry : componentPlacement.entrySet()) {
+
+            components.add(entry.getKey());
+
+            if (temp.containsKey(entry.getValue())) {
+                temp.get(entry.getValue()).add(entry.getKey());
+                continue;
+            }
+            Set<ComponentId> setToInsert = new HashSet<ComponentId>();
+            setToInsert.add(entry.getKey());
+            temp.put(entry.getValue(), setToInsert);
+
+        }
+
+        for (Map.Entry<DeviceId, Integer> entry : deviceTotalSlots.entrySet()) {
+            DeviceImpl deviceToAdd = new DeviceImpl(entry.getKey(), entry.getValue(),
+                    temp.getOrDefault(entry.getKey(), null), this);
+            devices.put(entry.getKey(), deviceToAdd);
+        }
+
+    }
+
+    public Set<DeviceImpl> getDevices(Set<DeviceId> ids) {
+        Set<DeviceImpl> result = new HashSet<DeviceImpl>();
+        for (DeviceId elem : ids) {
+            result.add(devices.get(elem));
+        }
+        return result;
+    }
+
+    public void acquireMutex() throws InterruptedException {
+
+        componentMutex.acquire();
+
+    }
+
+    public void releaseMutex() {
+
+        componentMutex.release();
+
+    }
+
+    public void addComponent(ComponentId comp) {
+        components.add(comp);
+    }
+
+    public void removeComponents(ComponentId comp) {
+        components.remove(comp);
+    }
+
+    public boolean doIHaveComponent(ComponentId comp) {
+        return components.contains(comp);
+    }
+
+    public void execute(ComponentTransfer transfer) throws TransferException {
+
+        try {
+
+            if (transfer.getComponentId() == null) {
+                throw new IllegalArgumentException();
+            }
+
+            ComponentId comp = transfer.getComponentId();
+
+            // Sprawdzamy czy transfer ma chociaż jeden DeviceId
+            if ((transfer.getSourceDeviceId() == null && transfer.getDestinationDeviceId() == null)) {
+                throw new IllegalTransferType(comp);
+            }
+
+            // Jeśli podano źródło to sprawdzamy czy istnieje
+            if (transfer.getSourceDeviceId() != null && !devices.containsKey(transfer.getSourceDeviceId())) {
+                throw new DeviceDoesNotExist(transfer.getSourceDeviceId());
+            }
+
+            // Jeśli podano cel to sprawdzamy czy istnieje
+            if (transfer.getDestinationDeviceId() != null && !devices.containsKey(transfer.getDestinationDeviceId())) {
+                throw new DeviceDoesNotExist(transfer.getDestinationDeviceId());
+            }
+
+            // sprawdzamy czy element musi być transferowany
+            if (transfer.getDestinationDeviceId() != null) {
+                DeviceImpl sourceDev = devices.get(transfer.getDestinationDeviceId());
+                if (sourceDev.doIHave(comp)) {
+                    throw new ComponentDoesNotNeedTransfer(comp,
+                            transfer.getDestinationDeviceId());
+                }
+            }
+
+            // sprawdzamy czy transfer dla komponentu został już zgłoczony
+            underTransferMutex.acquire();
+            if (underTransfer.contains(comp)) {
+                underTransferMutex.release();
+                throw new ComponentIsBeingOperatedOn(comp);
+            }
+            underTransfer.add(comp);
+            underTransferMutex.release();
+
+            operation opType;
+            DeviceImpl sourceDev = devices.getOrDefault(transfer.getSourceDeviceId(), null);
+            DeviceImpl destDev = devices.getOrDefault(transfer.getDestinationDeviceId(), null);
+
+            if (destDev == null) {
+                opType = operation.DELETE;
+            } else if (sourceDev == null) {
+                opType = operation.INSERT;
+            } else {
+                opType = operation.TRANSFER;
+            }
+
+            if (opType == operation.DELETE || opType == operation.TRANSFER) {
+                if (!sourceDev.doIHave(comp)) {
+                    throw new ComponentDoesNotExist(comp, sourceDev.id);
+                }
+            }
+
+            if (opType == operation.INSERT) {
+                componentMutex.acquire();
+                if (components.contains(comp)) {
+                    componentMutex.release();
+                    throw new ComponentAlreadyExists(comp);
+                }
+                componentMutex.release();
+            }
+
+            if (opType == operation.TRANSFER) {
+
+                componentMutex.acquire();
+                if (destDev.doIHave(comp)) {
+                    componentMutex.release();
+                    throw new ComponentDoesNotNeedTransfer(comp, destDev.id);
+                }
+                componentMutex.release();
+            }
+
+            switch (opType) {
+                case DELETE:
+
+                    // informujemy inne wątki że można zaczynać
+                    sourceDev.arriveOrReserve.release(comp, null, false);
+                    // przygotowywujemy transfer
+                    transfer.prepare();
+                    // Usuwamy element
+                    sourceDev.addOrRemove.remove(comp, true);
+                    // Wykonujemy transfer
+                    transfer.perform();
+
+                    break;
+
+                case INSERT:
+
+                    // Rezerwujemy miejsce na urządzeniu
+                    destDev.arriveOrReserve.reserve(comp, true);
+                    // rozpoczynamy transfer
+                    transfer.prepare();
+                    // Zajmujemy miejsce na dysku
+                    destDev.addOrRemove.add(comp, false);
+                    // Przenosimy dane
+                    transfer.perform();
+
+                    break;
+
+                case TRANSFER:
+                
+                    // Rezerwujemy miejsce na urządzeniu
+                    destDev.arriveOrReserve.reserve(comp, false);
+                    // informujemy inne wątki że można zaczynać
+                    sourceDev.arriveOrReserve.release(comp, null, false);
+                    // rozpoczynamy transfer
+                    transfer.prepare();
+                    // Usuwamy element
+                    sourceDev.addOrRemove.remove(comp, false);
+                    // Zajmujemy miejsce na dysku
+                    destDev.addOrRemove.add(comp, false);
+                    // Przenosimy dane
+                    transfer.perform();
+
+                    break;
+            }
+
+            underTransferMutex.acquire();
+            underTransfer.remove(comp);
+            underTransferMutex.release();
+
+        }
+
+        catch (InterruptedException e) {
+            throw new RuntimeException("panic: unexpected thread interruption");
+        }
+
+    }
+
+}
